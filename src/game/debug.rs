ab07b0c06aecd6bf3e2a400855772c6d3296737b
hheik <4469778+hheik@users.noreply.github.com>
2022-12-25 18:38:10 +0200

    disabled collision visualizer and inspector plugins

--- src/game/debug.rs
+++ src/game/debug.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
-use bevy_inspector_egui::*;
+// use bevy_inspector_egui::*;
 use bevy_prototype_debug_lines::DebugLinesPlugin;
-use bevy_rapier2d::prelude::*;
+// use bevy_rapier2d::prelude::*;
 
 mod terrain;
 
@@ -12,8 +12,8 @@ pub struct DebugPlugin;
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(DebugLinesPlugin::default())
-            .add_plugin(RapierDebugRenderPlugin::default())
-            .add_plugin(WorldInspectorPlugin::new())
+            // .add_plugin(RapierDebugRenderPlugin::default())
+            // .add_plugin(WorldInspectorPlugin::new())
             .add_plugin(TerrainDebugPlugin);
     }
 }
